934abbf30cbcf94932caa01f2694a5d872086728
coderrob22 <robertknott7@gmail.com>
2023-09-01 05:51:07 -0400

    set up path for register and login pages & created new app/module called accounts

--- btre/settings.py
+++ btre/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
