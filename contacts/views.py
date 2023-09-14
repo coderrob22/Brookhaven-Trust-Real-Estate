3f75b9e03eea20d5b598a2ef02c9ab9662c7fb62
coderrob22 <robertknott7@gmail.com>
2023-09-14 05:51:07 -0400

    email notifications set

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,9 +14,26 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # This will check if the logged in user has made an inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, "You have already made an inquiry for this property")
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+ 
+        # Send inquiry form to realtor
+        send_mail(
+            'Property Listing Inquiry', 
+            'Someone wants to learn more about ' + listing + '. Sign into the admin panel for more info.',
+            'myemail.somegmail.com',
+            ['realtor_email'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
