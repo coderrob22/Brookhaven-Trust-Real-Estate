86306ea458e97f133b5a61d8692c0bf3ac271e18
coderrob22 <robertknott7@gmail.com>
2023-09-25 04:43:03 -0400

    added whitenoise for static files

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-python manage.py collectstatic --noinput
 web: gunicorn btre.wsgi --log-file -
--- btre/settings.py
+++ btre/settings.py
@@ -47,11 +47,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic', 
     'django.contrib.humanize',
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +136,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
